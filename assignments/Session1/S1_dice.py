af12f1dc2dca46fa6eb2c3dde3d100acaaf43f51
Alex Chopard <alex.chopard1998@gmail.com>
2018-10-07 13:05:15 +0200

    Start the Dice Game

---
+++ assignments/Session1/S1_dice.py
@@ -0,0 +1,60 @@
+from enum import Enum
+
+class TypeOfPlayers (Enum):
+  PLAYER = 'Player'
+  COMPUTER = 'Computer'
+
+def gameIsEnded (scores):
+  if (scores[TypeOfPlayers.PLAYER] >= 100 or scores[TypeOfPlayers.COMPUTER] >= 100):
+    return True
+  else:
+    return False
+
+def switchPlayer (whoPlay):
+  if (whoPlay == TypeOfPlayers.PLAYER):
+    whoPlay = TypeOfPlayers.COMPUTER
+  else:
+    whoPlay = TypeOfPlayers.PLAYER
+  return whoPlay
+
+def diceGame ():
+  scores = { TypeOfPlayers.PLAYER: 0, TypeOfPlayers.COMPUTER: 0 }
+  whoPlay = TypeOfPlayers.COMPUTER
+
+  while not gameIsEnded(scores):
+    print('----------------------------------------------------------')
+    print('Scores :')
+    print('        - You : ' + str(scores[TypeOfPlayers.PLAYER]))
+    print('        - Computer : ' + str(scores[TypeOfPlayers.COMPUTER]) + '\n')
+
+    print('How play : ' + str(whoPlay.value))
+    scores[whoPlay] += 10
+    whoPlay = switchPlayer(whoPlay)
+
+  result = ''
+  if (gameIsEnded(scores) and scores[TypeOfPlayers.PLAYER] >= 100):
+    result = 'WIN '
+  else:
+    result = 'LOSE'
+
+  # Print the result of the game
+  print('*--------------------------------------------------------*')
+  print('\n|                       YOU ' + result + ' !                       |\n')
+  print('*--------------------------------------------------------*')
+  
+  return True
+
+# Start the game
+print('*--------------------------------------------------------*')
+print('|                                                        |')
+print('|              Welcome to the DICE GAME !                |')
+print('|                                                        |')
+print('*--------------------------------------------------------*')
+
+play = input('\nStart a new Game ? (y/n)\n')
+
+if (type(play) is str and (play == 'y' or play == 'Y' or play == 'yes' or play == 'Yes')):
+  diceGame()
+else:
+  print('See you soon ;)')
+
