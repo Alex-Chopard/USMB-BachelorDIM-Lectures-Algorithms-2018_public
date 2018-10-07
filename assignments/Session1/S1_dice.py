0e9b08fb1dd5b43f48cf2cdde8dc0ff8ffaa9c89
Alex Chopard <alex.chopard1998@gmail.com>
2018-10-07 18:33:08 +0200

    Add many comment in S1_dice.py

--- assignments/Session1/S1_dice.py
+++ assignments/Session1/S1_dice.py
@@ -18,28 +18,50 @@ def gameIsEnded (scores):
     @rtype: boolean
     @return: Return True if a player win the game, or False if no user win.
   """
+
   if (scores[TypeOfPlayers.PLAYER] >= MAX_SCORES or scores[TypeOfPlayers.COMPUTER] >= MAX_SCORES):
     return True
   else:
     return False
 
 def switchPlayer (whoPlay):
+  """
+    Function for switch the player
+
+    @type whoPlay: TypeOfPlayers
+    @param whoPlay: The current player
+
+    @rtype: TypeOfPlayers
+    @return: Return the new user who play
+  """
+
   if (whoPlay == TypeOfPlayers.PLAYER):
     whoPlay = TypeOfPlayers.COMPUTER
   else:
     whoPlay = TypeOfPlayers.PLAYER
+
   return whoPlay
 
 def playerPlayed ():
+  """
+    Function for manager the real player when he play
+
+    @rtype: int
+    @return: Return the scores optain by the player
+  """
+
   tourEnded = False
   currentScore = 0
 
   while not tourEnded:
     print('Current score : ' + str(currentScore))
     roll = input('Roll the dice ?(y/n)')
+
+    # Check if the player when to roll the dice one more time.
     if (type(roll) is str and (roll == 'y' or roll == 'Y' or roll == 'yes' or roll == 'Yes')):
       dice = random.randint(1, 6)
       print('Result : ' + str(dice))
+      # If the player optain 1, then exit the function
       if (dice == 1):
         tourEnded = True
         currentScore = 0
@@ -52,6 +74,13 @@ def playerPlayed ():
   return currentScore
 
 def diceGame ():
+  """
+    Function for manage all the game.
+
+    @rtype: None
+    @return: Return None when the game is ended
+  """
+  
   scores = { TypeOfPlayers.PLAYER: 0, TypeOfPlayers.COMPUTER: 0 }
   whoPlay = TypeOfPlayers.COMPUTER
 
@@ -81,7 +110,7 @@ def diceGame ():
   print('\n|                       YOU ' + result + ' !                       |\n')
   print('*--------------------------------------------------------*')
   
-  return True
+  return None
 
 # Start the game
 print('*--------------------------------------------------------*')
